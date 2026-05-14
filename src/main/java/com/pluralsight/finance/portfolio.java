12c65ce8bc810a40d0160cb3695b9e65b927e934
branRoberts <brandonkrob12@gmail.com>
2026-05-14 18:18:07 -0400

    Second Commit Copied from Raymond code and created a portfolio class and added code

--- src/main/java/com/pluralsight/finance/portfolio.java
+++ src/main/java/com/pluralsight/finance/portfolio.java
@@ -1,22 +1,41 @@
 package com.pluralsight.finance;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class portfolio {
     private String name;
     private String owner;
-    private List<BankAccount> assets;
+    private List<Valuable> assets;
 
     public portfolio(String name, String owner) {
         this.name = name;
         this.owner = owner;
+        this.assets = new ArrayList<>();
     }
-    public List<BankAccount> getAssets(Valuable valuable) {
-        return assets;
+    public void add(Valuable asset) {
+        assets.add(asset);
     }
     public double getValue(){
+        double valueTotal = 0;
+        for (Valuable asset : assets) {
+            valueTotal += asset.getValue();
+        }
+    return valueTotal; }
+    public Valuable getMostValuable(){
+        Valuable mostValuableAsset = assets.get(0);
+        double mostValuableAssetValue = mostValuableAsset.getValue();
+        for (Valuable asset : assets) {
+            double currentAssetValue = asset.getValue();
+            if (currentAssetValue > mostValuableAssetValue) {
+                mostValuableAsset = asset;
+                mostValuableAssetValue = currentAssetValue;
+            }
+        }
+    return mostValuableAsset;}
+    public Valuable getLeastValuable(){
+        Valuable leastValuableAsset = assets.get(0);
 
-    return; }
-    public
+    }
 }
 
