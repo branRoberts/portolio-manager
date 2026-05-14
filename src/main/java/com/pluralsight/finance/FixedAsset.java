7f2d152d8cf43faef0ad1201819e01c8d33683fa
branRoberts <brandonkrob12@gmail.com>
2026-05-14 12:44:44 -0400

    Second Commit Copied from Raymond code and created a portfolio class and added code

--- src/main/java/com/pluralsight/finance/FixedAsset.java
+++ src/main/java/com/pluralsight/finance/FixedAsset.java
@@ -1,4 +1,13 @@
 package com.pluralsight.finance;
 
-public class FixedAsset {
+public abstract class FixedAsset implements Valuable {
+    private String name;
+    private double value;
+
+    public FixedAsset(String name, double value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public abstract double getValue();
 }
---
+++ src/main/java/com/pluralsight/finance/portfolio.java
@@ -0,0 +1,22 @@
+package com.pluralsight.finance;
+
+import java.util.List;
+
+public class portfolio {
+    private String name;
+    private String owner;
+    private List<BankAccount> assets;
+
+    public portfolio(String name, String owner) {
+        this.name = name;
+        this.owner = owner;
+    }
+    public List<BankAccount> getAssets(Valuable valuable) {
+        return assets;
+    }
+    public double getValue(){
+
+    return; }
+    public
+}
+
